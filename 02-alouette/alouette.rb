3c27ddfccd1d6317b8d1789409c6e1e14eb29692
Tamiko Terada <tamikodevo@gmail.com>
2017-02-26 23:52:39 -0800

    change .verse to self.verse to match tests

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -1,5 +1,7 @@
 class Alouette
 
+  attr_accessor :verse_parts
+
   PARTS = [ "la tête", "le bec", "les yeux", "le cou", "les ailes",
     "les pattes" ]
 
@@ -9,14 +11,14 @@ class Alouette
     def self.lines_for_verse(number)
       verse_parts = Array.new
       # reverse order indices (stack)
-      for index in (number-1).downto(0)
+      for index in (number).downto(0)
         verse_parts << "Et #{PARTS[index]}!"
       end
-      return line_parts
+      return verse_parts
       # output(3): ["Et les yeux!", "Et le bec!", "Et la tête!"]
     end
 
-    def verse(number)
+    def self.verse(number)
       array_of_parts = lines_for_verse(number)
 
       verse_begin =
@@ -26,7 +28,6 @@ class Alouette
       2.times do
         verse_middle += "Je te plumerai #{array_of_parts[0]}.\n"
       end
-
       array_of_parts.each do |each_part|
         2.times do
           verse_middle += "#{each_part}\n"
