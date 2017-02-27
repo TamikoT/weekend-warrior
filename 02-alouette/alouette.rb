b986903954b6304d1564a199227c16866d137146
Tamiko Terada <tamikodevo@gmail.com>
2017-02-26 23:57:04 -0800

    self.verse omit refrain lines to pass meet 'first two lines' test

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -21,28 +21,25 @@ class Alouette
     def self.verse(number)
       array_of_parts = lines_for_verse(number)
 
-      verse_begin =
-      "Alouette, gentille alouette,\nAlouette, je te plumerai.\n\n"
-
-      verse_middle = ""
+      verse = ""
       2.times do
-        verse_middle += "Je te plumerai #{array_of_parts[0]}.\n"
+        verse += "Je te plumerai #{array_of_parts[0]}.\n"
       end
       array_of_parts.each do |each_part|
         2.times do
-          verse_middle += "#{each_part}\n"
+          verse += "#{each_part}\n"
         end
       end
 
       verse_end =
       "Alouette!\nAlouette!\nA-a-a-ah\n\n"
 
-      return verse_begin + verse_middle + verse_end
+      return verse + verse_end
     end
 
     def sing
     end
-
   end
 
-  puts Alouette.lines_for_verse(2)
+  # verse_begin =
+  # "Alouette, gentille alouette,\nAlouette, je te plumerai.\n\n"
