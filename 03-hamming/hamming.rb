1dfee1ae156a5e519f4f0db9971843fc2b7cfb0e
Tamiko Terada <tamikodevo@gmail.com>
2017-05-03 09:38:57 -0700

    add raise argumenterror to method, passes all tests

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,10 +1,11 @@
 class Hamming
   def self.compute(a,b)
+    unless a.length == b.length
+      raise ArgumentError
+    end
     count = 0
-    if a.length == b.length
-      (0...a.length).each do |i|
-        count += 1 if a[i] != b[i]
-      end
+    (0...a.length).each do |i|
+      count += 1 if a[i] != b[i]
     end
     return count
   end
