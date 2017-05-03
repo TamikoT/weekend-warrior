bc6941941943b1f1fb9a5f96af3abd31c0662850
Tamiko Terada <tamikodevo@gmail.com>
2017-05-03 09:37:16 -0700

    update method for  13 assertations, 1 failure

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,7 +1,11 @@
 class Hamming
   def self.compute(a,b)
+    count = 0
     if a.length == b.length
-      return 0
+      (0...a.length).each do |i|
+        count += 1 if a[i] != b[i]
+      end
     end
+    return count
   end
 end
